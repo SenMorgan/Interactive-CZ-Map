b98e407da80d5bb37ab973f576d2e9ac6f7a8480
Sen Morgan <technars@gmail.com>
2024-11-29 15:26:38 +0100

    feat: add WiFi manager for improved connection handling and modularity (used raw example with minimum changes)
    WiFi Manager repo: https://github.com/khoih-prog/ESPAsync_WiFiManager

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,23 @@
 #include "constants.h"
 #include "aws_iot.h"
 #include "leds.h"
+#include "wifi_manager.h"
 
 void setup()
 {
     Serial.begin(115200);
-    delay(10);
-    Serial.print(F("\n\nConnecting to " WIFI_SSID "..."));
+    while (!Serial)
+        delay(10);
 
-    // Set device as a Wi-Fi Station
-    WiFi.hostname(HOSTNAME);
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.print(F("\nWiFi connected!\nIP address: "));
-    Serial.println(WiFi.localIP());
-
+    // Initialize modules
+    initWiFiManager();
     initAWS();
     initLeds();
 }
 
 void loop()
 {
+    handleWiFi();
     refreshLeds();           // Update the state of the LEDs
     maintainAWSConnection(); // Maintain the MQTT connection
     periodicStatusPublish(); // Publish the device status periodically
---
+++ src/wifi_manager.h
@@ -0,0 +1,7 @@
+#ifndef WIFI_MANAGER_H
+#define WIFI_MANAGER_H
+
+void initWiFiManager();
+void handleWiFi();
+
+#endif // WIFI_MANAGER_H
